203126f80097df0e5c9eefbb36a936a9658a29fb
anandhu <anandhuzz916@gmail.com>
2026-02-22 19:33:44 +0530

    Initial commit with dependency

---
+++ src/test/java/com/spring/tradexeurekaserver/TradexEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tradexeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradexEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
